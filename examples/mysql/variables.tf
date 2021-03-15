fb8bcf8ad60ea7b9c3f19bf4a86830a8d703d82f
Young-ook Kim <youngook.kim@gmail.com>
2021-03-15 14:42:32 +0900

    fix(mysql): correct aws account variable in mysql example

--- examples/mysql/variables.tf
+++ examples/mysql/variables.tf
@@ -1,8 +1,8 @@
 # Variables for providing to module fixture codes
 
 ### aws credential
-variable "aws_account_id" {
-  description = "The aws account id for the tf backend creation (e.g. 857026751867)"
+variable "aws_account" {
+  description = "The aws account id for example (e.g. 857026751867)"
 }
 ### network
 variable "aws_region" {
