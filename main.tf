2ae0d2a08abbb315b81e300ebd40630ef25efb5b
Young-ook <youngook.kim@gmail.com>
2021-12-17 15:35:17 +0900

    feat(aurora): master user credential output (#23)
    * feat(aurora): master user credential output
    
    * feat(aurora): name and tag update

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ resource "random_password" "password" {
 
 # subnet group
 resource "aws_db_subnet_group" "db" {
-  name       = format("%s-db", var.name)
+  name       = format("%s-db", local.name)
   subnet_ids = local.subnet_ids
   tags       = merge(local.default-tags, var.tags)
 }
 
 # parameter groups
 resource "aws_rds_cluster_parameter_group" "db" {
-  name = format("%s-db-cluster-params", var.name)
+  name = format("%s-db-cluster-params", local.name)
   tags = merge(local.default-tags, var.tags)
 
   family = lookup(var.aurora_cluster, "family", local.default_cluster.family)
@@ -43,7 +43,7 @@ resource "aws_rds_cluster_parameter_group" "db" {
 
 resource "aws_db_parameter_group" "db" {
   for_each = { for key, val in var.aurora_instances : key => val }
-  name     = format("%s-db-instance-%s-params", var.name, each.key)
+  name     = format("%s-db-instance-%s-params", local.name, each.key)
   tags     = merge(local.default-tags, var.tags)
 
 
@@ -63,21 +63,22 @@ resource "aws_db_parameter_group" "db" {
 
 # rds cluster
 resource "aws_rds_cluster" "db" {
-  cluster_identifier_prefix       = format("%s-", var.name)
-  engine                          = lookup(var.aurora_cluster, "engine", local.default_cluster.engine)
-  engine_mode                     = lookup(var.aurora_cluster, "engine_mode", local.default_cluster.engine_mode)
-  engine_version                  = lookup(var.aurora_cluster, "version", local.default_cluster.version)
-  port                            = lookup(var.aurora_cluster, "port", local.default_cluster.port)
-  skip_final_snapshot             = lookup(var.aurora_cluster, "skip_final_snapshot", local.default_cluster.skip_final_snapshot)
-  database_name                   = lookup(var.aurora_cluster, "database", local.default_cluster.database)
-  master_username                 = lookup(var.aurora_cluster, "user", local.default_cluster.user)
-  master_password                 = random_password.password.result
-  snapshot_identifier             = lookup(var.aurora_cluster, "snapshot_id", local.default_cluster.snapshot_id)
-  backup_retention_period         = lookup(var.aurora_cluster, "backup_retention", local.default_cluster.backup_retention)
-  db_subnet_group_name            = aws_db_subnet_group.db.name
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db.name
-  vpc_security_group_ids          = coalescelist(aws_security_group.db.*.id, [])
-  tags                            = merge(local.default-tags, var.tags)
+  cluster_identifier_prefix           = format("%s-", local.name)
+  engine                              = lookup(var.aurora_cluster, "engine", local.default_cluster.engine)
+  engine_mode                         = lookup(var.aurora_cluster, "engine_mode", local.default_cluster.engine_mode)
+  engine_version                      = lookup(var.aurora_cluster, "version", local.default_cluster.version)
+  port                                = lookup(var.aurora_cluster, "port", local.default_cluster.port)
+  skip_final_snapshot                 = lookup(var.aurora_cluster, "skip_final_snapshot", local.default_cluster.skip_final_snapshot)
+  database_name                       = lookup(var.aurora_cluster, "database", local.default_cluster.database)
+  master_username                     = lookup(var.aurora_cluster, "user", local.default_cluster.user)
+  master_password                     = random_password.password.result
+  iam_database_authentication_enabled = lookup(var.aurora_cluster, "iam_auth_enabled", local.default_cluster.iam_auth_enabled)
+  snapshot_identifier                 = lookup(var.aurora_cluster, "snapshot_id", local.default_cluster.snapshot_id)
+  backup_retention_period             = lookup(var.aurora_cluster, "backup_retention", local.default_cluster.backup_retention)
+  db_subnet_group_name                = aws_db_subnet_group.db.name
+  db_cluster_parameter_group_name     = aws_rds_cluster_parameter_group.db.name
+  vpc_security_group_ids              = coalescelist(aws_security_group.db.*.id, [])
+  tags                                = merge(local.default-tags, var.tags)
 
   lifecycle {
     ignore_changes        = [snapshot_identifier, master_password]
