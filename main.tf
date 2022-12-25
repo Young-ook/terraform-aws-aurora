527502d958b66782711c07049d5c19f9560978a1
youngookkim <youngook.kim@gmail.com>
2022-12-25 09:56:00 +0900

    fix(rds): fix password generation rule (#45)

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ locals {
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = "‘~!@#$%^&*()_-+={}[]/<>,.;?':|"
+  override_special = "‘~!@#$%^*()_-+={}[],.;?':|"
 }
 
 # subnet group
