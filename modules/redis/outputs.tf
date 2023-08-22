5c0a9fc777b699b5eca3c206915cb43b9fe55a98
youngookkim <youngook.kim@gmail.com>
2023-08-22 10:52:48 +0900

    feat(redis): output variables (#55)

--- modules/redis/outputs.tf
+++ modules/redis/outputs.tf
@@ -4,3 +4,17 @@ output "cluster" {
   description = "Redis cluster"
   value       = aws_elasticache_replication_group.redis
 }
+
+output "endpoint" {
+  description = "The endpoints of Redis cluster"
+  value       = aws_elasticache_replication_group.redis.configuration_endpoint_address
+}
+
+output "user" {
+  description = "The primary user credential of the Redis cluster"
+  sensitive   = true
+  value = zipmap(
+    ["password"],
+    [local.password]
+  )
+}
